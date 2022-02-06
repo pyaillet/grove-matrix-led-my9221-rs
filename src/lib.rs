adab9fd37c5b78478bdadf79da0a7dcde8edb51a
Pierre-Yves Aillet <pyaillet@gmail.com>
2022-02-06 15:26:37 +0100

    :memo: add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,68 +2,122 @@
 
 use embedded_hal::blocking::i2c::{Read, Write};
 
+/// Default I2C Address for the grove matrix LED driver
 pub const DEFAULT_ADDRESS: u8 = 0x65;
 
 #[derive(Debug, Clone, Copy)]
 pub enum I2cCmd {
-    GetDevID = 0x00,           // This command gets device ID information
-    DispBar = 0x01,            // This command displays LED bar
-    DispEmoji = 0x02,          // This command displays emoji
-    DispNum = 0x03,            // This command displays number
-    DispStr = 0x04,            // This command displays string
-    DispCustom = 0x05,         // TODO: This command displays user-defined pictures
-    DispOff = 0x06,            // This command cleans the display
-    DispAscii = 0x07,          // not use
-    DispFlash = 0x08,          // TODO: This command displays pictures which are stored in flash
-    DispColorBar = 0x09,       // This command displays colorful led bar
-    DispColorWave = 0x0a,      // This command displays built-in wave animation
-    DispColorClockWise = 0x0b, // This command displays built-in clockwise animation
-    DispColorAnimation = 0x0c, // This command displays other built-in animation
-    DispColorBlock = 0x0d,     // This command displays an user-defined color
+    /// This command gets device ID information
+    GetDevID = 0x00,
+    /// This command displays LED bar
+    DispBar = 0x01,
+    /// This command displays emoji
+    DispEmoji = 0x02,
+    /// This command displays number
+    DispNum = 0x03,
+    /// This command displays string
+    DispStr = 0x04,
+    /// TODO: This command displays user-defined pictures
+    DispCustom = 0x05,
+    /// This command cleans the display
+    DispOff = 0x06,
+    /// not use
+    DispAscii = 0x07,
+    /// TODO: This command displays pictures which are stored in flash
+    DispFlash = 0x08,
+    /// This command displays colorful led bar
+    DispColorBar = 0x09,
+    /// This command displays built-in wave animation
+    DispColorWave = 0x0a,
+    /// This command displays built-in clockwise animation
+    DispColorClockWise = 0x0b,
+    /// This command displays other built-in animation
+    DispColorAnimation = 0x0c,
+    /// This command displays an user-defined color
+    DispColorBlock = 0x0d,
 
     ContinueData = 0x81,
 
-    StoreFlash = 0xa0,  // TODO: This command stores frames in flash
-    DeleteFlash = 0xa1, // TODO: This command deletes all the frames in flash
+    /// TODO: This command stores frames in flash
+    StoreFlash = 0xa0,
+    /// TODO: This command deletes all the frames in flash
+    DeleteFlash = 0xa1,
 
-    LedOn = 0xb0,        // This command turns on the indicator LED flash mode
-    LedOff = 0xb1,       // This command turns off the indicator LED flash mode
-    AutoSleepOn = 0xb2,  // This command enable device auto sleep mode
-    AutoSleepOff = 0xb3, // This command disable device auto sleep mode (default mode)
+    /// This command turns on the indicator LED flash mode
+    LedOn = 0xb0,
+    /// This command turns off the indicator LED flash mode
+    LedOff = 0xb1,
+    /// This command enable device auto sleep mode
+    AutoSleepOn = 0xb2,
+    /// This command disable device auto sleep mode (default mode)
+    AutoSleepOff = 0xb3,
 
-    DispRotate = 0xb4, // This command setting the display orientation
-    DispOffset = 0xb5, // This command setting the display offset
+    /// This command setting the display orientation
+    DispRotate = 0xb4,
+    /// This command setting the display offset
+    DispOffset = 0xb5,
 
-    SetAddress = 0xc0,     // TODO: This command sets device i2c address
-    ResetAddress = 0xc1,   // TODO: This command resets device i2c address
-    TestTXRXOn = 0xe0,     // This command enable TX RX pin test mode
-    TestTXRXOff = 0xe1,    // This command disable TX RX pin test mode
-    TestGetVersion = 0xe2, // This command use to get software version
-    GetDeviceUID = 0xf1,   // This command use to get chip id
+    /// TODO: This command sets device i2c address
+    SetAddress = 0xc0,
+    /// TODO: This command resets device i2c address
+    ResetAddress = 0xc1,
+    /// This command enable TX RX pin test mode
+    TestTXRXOn = 0xe0,
+    /// This command disable TX RX pin test mode
+    TestTXRXOff = 0xe1,
+    /// This command use to get software version
+    TestGetVersion = 0xe2,
+    /// This command use to get chip id
+    GetDeviceUID = 0xf1,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum DisplayRotate {
+    /// No rotation
     Deg0 = 0,
+    /// Rotate 90 degrees
     Deg90 = 1,
+    /// Rotate 180 degrees
     Deg180 = 2,
+    /// Rotate 270 degrees
     Deg270 = 3,
 }
 
 #[derive(Debug, Clone, Copy)]
+pub enum ColorAnimation {
+    BigClockWise = 0,
+    SmallClockWise = 1,
+    RainbowCycle = 2,
+    Fire = 3,
+    Walking = 4,
+    BrokenHeart = 5,
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum Colors {
+    /// Red
     Red = 0x00,
+    /// Orange
     Orange = 0x12,
+    /// Yellow
     Yellow = 0x18,
+    /// Green
     Green = 0x52,
+    /// Cyan
     Cyan = 0x7f,
+    /// Blue
     Blue = 0xaa,
+    /// Purple
     Purple = 0xc3,
+    /// Pink
     Pink = 0xdc,
+    /// White
     White = 0xfe,
+    /// Black
     Black = 0xff,
 }
 
+/// The grove matrix LED driver
 pub struct My9221LedMatrix<I2C: Write> {
     address: u8,
     i2c: I2C,
@@ -90,10 +144,23 @@ impl<I2C> My9221LedMatrix<I2C>
 where
     I2C: Write + Read,
 {
+    /// Create a new instance of the grove matrix LED driver
+    ///
+    /// # Arguments
+    ///
+    /// * `i2c` - The I2C peripheral to use
+    /// * `address` - The I2C address to use (default is 0x65)
+    ///
     pub fn new(address: u8, i2c: I2C) -> Self {
         Self { address, i2c }
     }
 
+    /// Rotate the display
+    ///
+    /// # Arguments
+    ///
+    /// * `rotate` - The display orientation
+    ///
     pub fn set_led_matrix_rotate(
         &mut self,
         rotate: DisplayRotate,
@@ -107,7 +174,8 @@ where
         Ok(())
     }
 
-    pub fn stop_displat(&mut self) -> Result<(), My9221LedMatrixError> {
+    /// Stop the display
+    pub fn stop_display(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 1];
         buf[0] = I2cCmd::DispOff as u8;
         self.i2c
@@ -116,6 +184,12 @@ where
         Ok(())
     }
 
+    /// Set the display offset
+    ///
+    /// # Arguments
+    ///
+    /// * `offset` - The display offset (x, y)
+    ///
     pub fn set_led_matrix_offset(&mut self, offset: (u8, u8)) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 3];
         buf[0] = I2cCmd::DispOffset as u8;
@@ -127,6 +201,7 @@ where
         Ok(())
     }
 
+    /// Turn on the display
     pub fn turn_on_led_flash(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 1];
         buf[0] = I2cCmd::LedOn as u8;
@@ -136,6 +211,7 @@ where
         Ok(())
     }
 
+    /// Turn off the display
     pub fn turn_off_led_flash(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 1];
         buf[0] = I2cCmd::LedOff as u8;
@@ -145,6 +221,7 @@ where
         Ok(())
     }
 
+    /// Enable auto sleep mode
     pub fn enable_auto_sleep(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 1];
         buf[0] = I2cCmd::AutoSleepOn as u8;
@@ -154,6 +231,7 @@ where
         Ok(())
     }
 
+    /// Disable auto sleep mode
     pub fn disable_auto_sleep(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf = [0; 1];
         buf[0] = I2cCmd::AutoSleepOff as u8;
@@ -163,6 +241,15 @@ where
         Ok(())
     }
 
+    /// Display a bar
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * `bar` - The bar to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
+    /// * `color` - The color of the bar
     pub fn display_bar(
         &mut self,
         bar: u8,
@@ -184,6 +271,14 @@ where
         Ok(())
     }
 
+    /// Display an Emoji
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * `emoji` - The emoji to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_emoji(
         &mut self,
         emoji: u8,
@@ -203,6 +298,15 @@ where
         Ok(())
     }
 
+    /// Display an number
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * `number` - The number to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
+    /// * `color` - The color of the number
     pub fn display_number(
         &mut self,
         number: u16,
@@ -225,6 +329,13 @@ where
         Ok(())
     }
 
+    /// Display a string
+    ///
+    /// # Arguments
+    /// * `string` - The string to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
+    /// * `color` - The color of the string
     pub fn display_string(
         &mut self,
         string: &str,
@@ -256,6 +367,12 @@ where
         Ok(())
     }
 
+    /// Display a color block
+    ///
+    /// # Arguments
+    /// * `rgb` - The color to display in RGB format (0x00RRGGBB)
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_color_block(
         &mut self,
         rgb: u32,
@@ -278,6 +395,12 @@ where
         Ok(())
     }
 
+    /// Display a color bar
+    ///
+    /// # Arguments
+    /// * `bar` - the color bar to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_color_bar(
         &mut self,
         bar: u8,
@@ -298,6 +421,12 @@ where
         Ok(())
     }
 
+    /// Display a color wave
+    ///
+    /// # Arguments
+    /// * `wave` - the color wave to display
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_color_wave(
         &mut self,
         wave: u8,
@@ -318,6 +447,14 @@ where
         Ok(())
     }
 
+    /// Display a color clockwise
+    ///
+    /// # Arguments
+    ///
+    /// * `clockwise` - If true, the color will be displayed clockwise, if false, anti-clockwise
+    /// * `big` - If true, the color clockwise will be displayed in big size, if false, small size
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_color_clockwise(
         &mut self,
         clockwise: bool,
@@ -340,9 +477,21 @@ where
         Ok(())
     }
 
+    /// Display a color animation
+    ///
+    /// # Arguments
+    /// * `animation` - The animation to display
+    ///   - `ColorAnimation::BigClockWise`
+    ///   - `ColorAnimation::SmallClockWise`
+    ///   - `ColorAnimation::RainbowCycle`
+    ///   - `ColorAnimation::Fire`
+    ///   - `ColorAnimation::Walking`
+    ///   - `ColorAnimation::BrokenHeart`
+    /// * `duration_time` - The duration time of the bar
+    /// * `forever_flag` - If true, the bar will be displayed forever
     pub fn display_color_animation(
         &mut self,
-        animation_index: u8,
+        animation_index: ColorAnimation,
         duration_time: u16,
         forever_flag: bool,
     ) -> Result<(), My9221LedMatrixError> {
@@ -350,13 +499,12 @@ where
 
         buf[0] = I2cCmd::DispColorAnimation as u8;
         (buf[1], buf[2]) = match animation_index {
-            0 => (0, 28),    // big clockwise
-            1 => (29, 41),   // small clockwise
-            2 => (255, 255), // rainbow cycle
-            3 => (254, 254), // fire
-            4 => (42, 43),   // walking
-            5 => (44, 52),   // broken heart
-            _ => (0, 0),     // None
+            ColorAnimation::BigClockWise => (0, 28),    // big clockwise
+            ColorAnimation::SmallClockWise => (29, 41), // small clockwise
+            ColorAnimation::RainbowCycle => (255, 255), // rainbow cycle
+            ColorAnimation::Fire => (254, 254),         // fire
+            ColorAnimation::Walking => (42, 43),        // walking
+            ColorAnimation::BrokenHeart => (44, 52),    // broken heart
         };
         buf[3] = (duration_time & 0xff) as u8;
         buf[4] = ((duration_time >> 8) & 0xff) as u8;
@@ -368,6 +516,7 @@ where
         Ok(())
     }
 
+    /// Enable test mode
     pub fn enable_test_mode(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf: [u8; 1] = [0; 1];
 
@@ -379,6 +528,7 @@ where
         Ok(())
     }
 
+    /// Disable test mode
     pub fn disable_test_mode(&mut self) -> Result<(), My9221LedMatrixError> {
         let mut buf: [u8; 1] = [0; 1];
 
@@ -390,6 +540,7 @@ where
         Ok(())
     }
 
+    /// Test getting the version
     pub fn test_get_version(&mut self) -> Result<u32, My9221LedMatrixError> {
         let mut cmd: [u8; 1] = [0; 1];
         let mut buf: [u8; 4] = [0; 4];
@@ -406,6 +557,7 @@ where
         Ok((buf[0] as u32) << 24 | (buf[1] as u32) << 16 | (buf[2] as u32) << 8 | (buf[3] as u32))
     }
 
+    /// Get the device ID
     pub fn get_device_id(&mut self) -> Result<u8, My9221LedMatrixError> {
         let mut cmd: [u8; 1] = [0; 1];
         let mut buf: [u8; 1] = [0; 1];
